d2ee13685d3da510a1e568ffbc067184bb2acd1f
Sebastien Jarjanette <sjarj@MacBook-Pro-de-Sebastien.local>
2019-05-24 17:04:01 +0200

    Remove log

--- src/reducers/reducer_countries.js
+++ src/reducers/reducer_countries.js
@@ -3,7 +3,6 @@ import { GET_COUNTRIES } from '../actions';
 const initialState = { countries: [] };
 
 export default (state = initialState, action) => {
-  console.log(state);
   switch (action.type) {
     case GET_COUNTRIES:
       return { ...state, countries: action.payload };
