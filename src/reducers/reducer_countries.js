3877bff9ee92ea3959a52bead27b418b02b83d1a
Sebastien Jarjanette <sjarj@MacBook-Pro-de-Sebastien.local>
2019-05-26 15:40:22 +0200

    filtrer countries array

--- src/reducers/reducer_countries.js
+++ src/reducers/reducer_countries.js
@@ -5,9 +5,24 @@ const initialState = { countries: [] };
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_COUNTRIES:
-      return { ...state, countries: action.payload };
+      return { ...state, countries: getCountriesInfo(action.payload) };
 
     default:
       return state;
   }
+};
+
+const getCountriesInfo = data => {
+  return data
+    .map(c => {
+      return {
+        name: c.name,
+        currencyCode: c.currencies[0].code,
+        flag: c.flag,
+        code: c.alpha3Code,
+      };
+    })
+    .filter(c => {
+      c.currencyCode != `USD`;
+    });
 };
