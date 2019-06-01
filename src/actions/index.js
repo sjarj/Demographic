4aa4131851e97f5ad7d0fe2d538b3def1e49c672
Sebastien Jarjanette <sjarj@MacBook-Pro-de-Sebastien.local>
2019-06-01 16:30:10 +0200

    Action fetchRateExange

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const GET_COUNTRIES = `GET_COUNTRIES`;
+export const GET_RATE_EXANGE = `GET_RATE_EXANGE`;
 
 export const fetchCountries = () => {
   return dispatch => {
@@ -8,4 +9,31 @@ export const fetchCountries = () => {
       dispatch({ type: GET_COUNTRIES, payload: axiosResponse.data });
     });
   };
+};
+
+export const fetchRateExange = country => {
+  return dispatch => {
+    axios
+      .get(
+        `https://api.exchangeratesapi.io/history?start_at=${getLastMonth()}&end_at=${formatDate(
+          new Date()
+        )}&base=USD&symbols=${country.currentcyCode}`
+      )
+      .then(axiosResponse => {
+        dispatch({
+          type: GET_RATE_EXANGE,
+          payload: { rate: axiosResponse.data.rates, ...country },
+        });
+      });
+  };
+};
+
+const formatDate = date => {
+  return date.toISOString().split(`T`)[0];
+};
+
+const getLastMonth = () => {
+  let now = new Date();
+  now.setMonth(now.getMonth() - 1);
+  return formatDate(now);
 };
