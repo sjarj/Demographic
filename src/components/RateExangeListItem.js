fc835058dfd56d6cc75baf8e1f94c49356d432bc
Sebastien Jarjanette <sjarj@MacBook-Pro-de-Sebastien.local>
2019-06-07 20:45:52 +0200

    Show graphique

--- src/components/RateExangeListItem.js
+++ src/components/RateExangeListItem.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import reactChartkick, { AreaChart } from 'react-chartkick';
+import Chart from 'chart.js';
 
+reactChartkick.addAdapter(Chart);
 export default class RateExangeListItem extends Component {
+  formatData = (rates, currencyCode) => {
+    return Object.keys(rates).map(date => {
+      return [date, rates[date][currencyCode]];
+    });
+  };
+
   render() {
-    const { name, flag, rates } = this.props.rateExchange;
+    const { name, flag, rates, currencyCode } = this.props.rateExchange;
     return (
       <tr>
         <td>
@@ -11,11 +20,17 @@ export default class RateExangeListItem extends Component {
           <img
             src={flag}
             alt="flag"
-            style={{ width: `16rem`, height: `16rem` }}
+            style={{ width: `100px`, height: `60px` }}
           />
         </td>
 
-        <td>Graphique</td>
+        <td className="col-md-12">
+          <AreaChart
+            data={this.formatData(rates, currencyCode)}
+            xtitle="Date"
+            ytitle={currencyCode}
+          />
+        </td>
       </tr>
     );
   }
